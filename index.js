433334cf5ae047c2adf3a84cae59b62c406a34ec
Brooks Gunn <brooks.a.gunn@gmail.com>
2023-02-28 13:24:50 -0500

    Add basic hex value generation from array

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 let mainButton = document.querySelector(".btn");
 
+const hexInts = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
+
 mainButton.addEventListener("click", (e) => {
-  console.log("Y");
+  // let hexValue = ;
+  console.log(hexInts[Math.floor(Math.random() * hexInts.length)]);
 });
